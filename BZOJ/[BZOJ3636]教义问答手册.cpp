e0a0bf7465a8b4978b8fe056b9bc0055f322d0f2
nadal295704@126.com <nadal295704@126.com>
2018-10-15 17:16:01 +0800

    modify problem list

--- BZOJ/[BZOJ3636]教义问答手册.cpp
+++ BZOJ/[BZOJ3636]教义问答手册.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define pLL pair<LL,LL>
 #define pii pair<double,double>
 #define LOWBIT(x) x & (-x)
-#define LOCAL true
+// #define LOCAL true
 
 const int INF=2e9;
 const LL LINF=2e16;
@@ -94,6 +94,8 @@ int n,L,Q;
 int a[MAXN+48],sum[MAXN+48];
 int ans[MAXN+48];
 
+inline int calc(int left,int right) {return sum[right]-sum[left-1];}
+
 struct Query
 {
 	int l,r,ind;
@@ -101,11 +103,17 @@ struct Query
 }q[MAXN+48],tmp1[MAXN+48],tmp2[MAXN+48],tmp3[MAXN+48];
 int tot1,tot2,tot3;
 
-int dp1[MAXN+48][51],dp2[MAXN+48][51];
+int dp1[51][MAXN+48],dp2[51][MAXN+48];
 
 inline void solve(int l,int r,int ql,int qr)
 {
-	if (l>r || ql>qr) return;
+	if (l>r) return;
+	if (l==r)
+	{
+		for (register int i=ql;i<=qr;i++)
+			ans[q[i].ind]=((a[l]>=0 && L<=1)?a[l]:0);
+		return;
+	}
 	int mid=(l+r)>>1;tot1=tot2=tot3=0;
 	for (register int i=ql;i<=qr;i++)
 	{
@@ -113,12 +121,42 @@ inline void solve(int l,int r,int ql,int qr)
 		if (q[i].l>=mid+1) {tmp2[++tot2]=q[i];continue;}
 		tmp3[++tot3]=q[i];
 	}
-	int pt=l,ll=ql+tot1-1,rr=qr-tot2+1;
-	for (register int i=1;i<=tot1;i++) Q[pt++]=tmp1[i];
-	for (register int i=1;i<=tot3;i++) Q[pt++]=tmp3[i];
-	for (register int i=1;i<=tot2;i++) Q[pt++]=tmp2[i];
-	solve(l,mid,ql,ll);solve(mid+1,r,rr,qr);
+	int pt=ql,ll=ql+tot1-1,rr=qr-tot2+1;
+	for (register int i=1;i<=tot1;i++) q[pt++]=tmp1[i];
+	for (register int i=1;i<=tot3;i++) q[pt++]=tmp3[i];
+	for (register int i=1;i<=tot2;i++) q[pt++]=tmp2[i];
 	for (register int gap=0;gap<=L-1;gap++)
+	{
+		dp1[gap][mid-gap+1]=0;
+		for (register int i=mid-gap;i>=l;i--)
+		{
+			dp1[gap][i]=dp1[gap][i+1];
+			if (mid-gap-i+1>=L) check_max(dp1[gap][i],dp1[gap][i+L]+calc(i,i+L-1));
+		}
+	}
+	for (register int gap=0;gap<=L-1;gap++)
+	{
+		dp2[gap][mid+gap]=0;
+		for (register int i=mid+1+gap;i<=r;i++)
+		{
+			dp2[gap][i]=dp2[gap][i-1];
+			if (i-(mid+1+gap)+1>=L) check_max(dp2[gap][i],dp2[gap][i-L]+calc(i-L+1,i));
+		}
+	}
+	for (register int i=1;i<=tot3;i++)
+	{
+		ans[tmp3[i].ind]=dp1[0][tmp3[i].l]+dp2[0][tmp3[i].r];
+		for (register int j=mid;j>=mid-(L-1)+1;j--)
+			if (j>=tmp3[i].l && j+L-1<=tmp3[i].r)
+				check_max(ans[tmp3[i].ind],calc(j,j+L-1)+dp1[mid-j+1][tmp3[i].l]+dp2[j+L-1-(mid+1)+1][tmp3[i].r]);
+	}
+	for (register int gap=0;gap<=L-1;gap++)
+		for (register int i=mid-gap+1;i>=l;i--)
+			dp1[gap][i]=0;
+	for (register int gap=0;gap<=L-1;gap++)
+		for (register int i=mid+gap;i<=r;i++)
+			dp2[gap][i]=0;
+	solve(l,mid,ql,ll);solve(mid+1,r,rr,qr);
 }
 
 int main ()
@@ -132,7 +170,7 @@ int main ()
 	io.Get(n);io.Get(L);
 	for (register int i=1;i<=n;i++) io.Get(a[i]),sum[i]=sum[i-1]+a[i];
 	io.Get(Q);
-	for (register int i=1;i<=Q;i++) q[i].input();
+	for (register int i=1;i<=Q;i++) q[i].input(),q[i].ind=i;
 	solve(1,n,1,Q);
 	for (register int i=1;i<=Q;i++) io.Print(ans[i],'\n');
 	io.flush();
