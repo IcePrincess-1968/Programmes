a1751a01157199b0d50477abbdee67cdde78ab16
nadal295704@126.com <nadal295704@126.com>
2018-12-10 16:36:38 +0800

    add apc001g(unfinished)

--- AtCoder/[APC001G]Colorful Doors.cpp
+++ AtCoder/[APC001G]Colorful Doors.cpp
@@ -88,16 +88,18 @@ inline void Sub(int &x,int y,int MO) {x=sub(x-y,MO);}
 template<typename T> inline int quick_pow(int x,T y) {int res=1;while (y) {if (y&1) res=1ll*res*x%MOD;x=1ll*x*x%MOD;y>>=1;}return res;}
 template<typename T> inline int quick_pow(int x,T y,int MO) {int res=1;while (y) {if (y&1) res=1ll*res*x%MO;x=1ll*x*x%MO;y>>=1;}return res;}
 
-const int MAXN=1e5;
+const int MAXN=2e5;
 
-int n;
+int n,N;
 char s[MAXN+48];int a[MAXN+48],id[MAXN+48];
+int ans[MAXN+48];
+
+vector<int> boundary,inner;
 
 namespace allone
 {
     inline void solve()
     {
-        n*=2;
         if (n%4==0)
         {
             printf("Yes\n");
@@ -110,8 +112,49 @@ namespace allone
 
 namespace four
 {
+    vector<int> free;
     inline void solve()
     {
+        int ind=1;
+        for (register int i=0;i<int(inner.size());i+=4,ind+=2)
+            ans[inner[i]]=ans[inner[i+2]]=ind,ans[inner[i+1]]=ans[inner[i+3]]=ind+1;
+        assert(int(boundary.size())%2==0);
+        for (register int i=1;i<int(boundary.size())-1;i+=2,ind++)
+            ans[boundary[i]]=ans[boundary[i+1]]=ind;
+        free.clear();for (register int i=1;i<=n;i++) if (!ans[i]) free.pb(i);
+        for (register int i=0;i<int(free.size());i+=2,ind++)
+            ans[free[i]]=ans[free[i+1]]=ind;
+        printf("Yes\n");
+        for (register int i=1;i<=n;i++) printf("%d ",ans[i]);
+        puts("");
+    }
+}
+
+namespace two
+{
+    inline void solve()
+    {
+        int nonzero=0,ind=1;
+        for (register int i=1;i<=N-1;i++)
+            if ((i==1 && a[i]==1) || (a[i]==1 && a[i-1]==0))
+                if (a[i+1]==1) nonzero++;
+        if (nonzero<=1) {puts("No");return;}
+        int pt1,pt2,pt3,last;
+        for (register int i=1;i<=N-1;i++)
+            if (((i==1 && a[i]==1) || (a[i]==1 && a[i-1]==0)) && a[i+1]==1) {pt1=i;break;}
+        for (pt2=pt1+1;;pt2++)
+            if ((a[pt2]==1 && a[pt2-1]==0) && a[pt2+1]==1) break;
+        ans[id[pt1+1]]=ans[id[pt2+1]]=ind++;
+        for (pt3=pt2;a[pt3] && pt3<=N;pt3++);
+        ans[id[pt2]]=ans[id[pt3]]=ind++;
+        for (register int i=pt1+1;i<pt2;i++)
+        {
+            if (a[i-1]==0 && a[i]==1) ans[last]=ans[id[i]]=ind++;
+            if (a[i-1]==1 && a[i]==0) last=id[i];
+        }
+        boundary.clear();inner.clear();
+        boundary.pb(id[1]);
+        for (register int i=2;i<pt1;i++)
     }
 }
 
@@ -123,14 +166,22 @@ int main ()
     freopen ("a.out","w",stdout);
     cerr<<"Running..."<<endl;
 #endif
-    io.Get(n);n*=2;io.getstring(s+1);
-    a[1]=1;for (register int i=1;i<=n-1;i++) a[i+1]=(s[i]=='1');
+    io.Get(n);n*=2;N=n-1;io.getstring(s+1);
+    a[n]=1;for (register int i=1;i<=n-1;i++) a[i]=(s[i]=='1');
     int pt;for (pt=n;pt>=1 && a[pt]==1;pt--);
     if (!pt) {allone::solve();return 0;}
     for (register int i=pt;i>=1;i--) a[n-pt+i]=a[i];
     for (register int i=1;i<=n-pt;i++) a[i]=1;
-    for (register int i=1;i<=n
-    int cnt=0;for (register int i=2;i<=n-1;i++) if (a[i]==1 && a[i-1]==1 && a[i+1]==1) cnt++;
+    for (register int i=1;i<=n-pt;i++) id[i]=i+pt;for (register int i=n-pt+1,j=1;j<=pt;i++,j++) id[i]=j;
+    boundary.clear();inner.clear();
+    boundary.pb(id[1]);
+    for (register int i=1;i<=N-1;i++)
+    {
+        if (a[i] && a[i+1]) {inner.pb(id[i+1]);continue;}
+        if (a[i] || a[i+1]) {boundary.pb(id[i+1]);continue;}
+    }
+    boundary.pb(id[n]);
+    int cnt=int(inner.size());
     if (cnt&1) {printf("No\n");return 0;}
     if (cnt%4==0) four::solve(); else two::solve();
     io.flush();
