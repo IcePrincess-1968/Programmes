a90902fa6f153bda032c0fe6a9d2084f2aa78d32
IcePrincess_1968 <nadal295704@126.com>
2018-12-18 23:36:41 +0800

    finish agc029e

--- AtCoder/[AGC029E]Wandering TKHS.cpp
+++ AtCoder/[AGC029E]Wandering TKHS.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define pLL pair<LL,LL>
 #define pii pair<double,double>
 #define LOWBIT(x) x & (-x)
-#define LOCAL true
+// #define LOCAL true
 
 const int INF=2e9;
 const LL LINF=2e16;
@@ -93,13 +93,39 @@ const int MAXN=2e5;
 int n,maxn[MAXN+48],fa[MAXN+48];vector<int> v[MAXN+48];
 int dp[MAXN+48],ans[MAXN+48];
 
+int L[MAXN+48],R[MAXN+48],ind;
+
+int id[MAXN+48];
+inline bool cmp(int x,int y)
+{
+    int xx=fa[fa[x]],yy=fa[fa[y]];
+    return maxn[xx]<maxn[yy];
+}
+
+bool exist[MAXN+48];
+
+namespace DSU
+{
+    int pre[MAXN+48];
+    inline void init() {for (register int i=1;i<=n;i++) pre[i]=i;}
+    inline int find_anc(int x) {if (pre[x]!=x) pre[x]=find_anc(pre[x]);return pre[x];}
+    inline void update(int x,int y) {x=find_anc(x);y=find_anc(y);pre[x]=y;}
+}
+
+namespace BIT
+{
+    int c[MAXN+48];
+    inline void init() {memset(c,0,sizeof(c));}
+    inline void update(int x,int delta) {if (!x) return;while (x<=n) c[x]+=delta,x+=LOWBIT(x);}
+    inline int query(int x) {int res=0;while (x) res+=c[x],x^=LOWBIT(x);return res;}
+    inline int calc(int x,int y) {return query(y)-query(x-1);}
+}
+
 inline void dfs(int cur,int father)
 {
-    fa[cur]=father;
-    for (auto y : v[cur]) if (y!=father) maxn[y]=max(y,maxn[cur]),dfs(y,cur);
-    if (cur>maxn[fa[father]]) {dp[cur]=0;return;}
-    dp[cur]=1;
-    for (auto y : v[cur]) if (y!=father) dp[cur]+=dp[y];
+    L[cur]=++ind;fa[cur]=father;
+    for (auto y : v[cur]) if (y!=father) maxn[y]=max(maxn[cur],y),dfs(y,cur);
+    R[cur]=ind;
 }
 
 inline void Dfs(int cur,int father,int curans)
@@ -118,13 +144,35 @@ int main ()
     freopen ("a.out","w",stdout);
     cerr<<"Running..."<<endl;
 #endif
-    int x,y;scanf("%d",&n);
+    scanf("%d",&n);
     for (register int i=1;i<=n-1;i++)
     {
-        scanf("%d%d",&x,&y);
+        int x,y;scanf("%d%d",&x,&y);
         v[x].pb(y);v[y].pb(x);
     }
     maxn[1]=1;dfs(1,0);
+    for (register int i=1;i<=n;i++) id[i]=i;
+    maxn[0]=INF;sort(id+1,id+n+1,cmp);
+    DSU::init();BIT::init();int curpt=0;
+    for (register int i=1;i<=n;i++)
+    {
+        if (fa[fa[id[i]]]==0) continue;
+        int need=maxn[fa[fa[id[i]]]];
+        while (curpt<need-1)
+        {
+            ++curpt;exist[curpt]=true;
+            BIT::update(L[curpt],1);BIT::update(L[fa[curpt]],-1);
+            for (auto y : v[curpt])
+                if (exist[y])
+                {
+                    int xx=curpt,yy=y;if (fa[xx]==yy) swap(xx,yy);
+                    int ynum=BIT::calc(L[yy],R[yy]),anc=DSU::find_anc(xx);
+                    BIT::update(L[xx],ynum);BIT::update(L[fa[anc]],-ynum);
+                    DSU::update(yy,xx);
+                }
+        }
+        dp[id[i]]=BIT::calc(L[id[i]],R[id[i]]);
+    }
     Dfs(1,0,0);
     for (register int i=2;i<=n;i++) printf("%d ",ans[i]-1);
     puts("");
